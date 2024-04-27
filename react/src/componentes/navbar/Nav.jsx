2616fc20ce20c4db90145617c3cc5228e0896f9d
kevin <kevinbrrs96@gmail.com>
2024-04-27 16:57:59 -0300

    Se corrigio responsive

--- react/src/componentes/navbar/Nav.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Nav.module.css'
-function Nav() {
-    return (
-        <div className='container'>Nav</div>
-    )
-}
-
-export default Nav
--- react/src/componentes/navbar/Navbar.jsx
+++ react/src/componentes/navbar/Navbar.jsx
@@ -19,7 +19,9 @@ function Navbar() {
                 {/* <img className={stylos.imgLogo} src="https://media2.giphy.com/media/QssGEmpkyEOhBCb7e1/giphy.gif?cid=ecf05e47a0n3gi1bfqntqmob8g9aid1oyj2wr3ds3mg700bl&rid=giphy.gif" /> */}
                 <img className={stylos.imgLogo} src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjhmd3U2NXR5czhvbzRkczR5bTZtYmR5NHpmYmZyeGVmMTI3MjVncSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9dHM/kAm4u0lhDCmXnugz6p/giphy.gif" />
             </div>
-
+            <div className={`${stylos.responsive} ${showNav ? stylos.hide : ''}`} onClick={toggleNav}>
+                <FontAwesomeIcon icon={faBars} />
+            </div>
             <nav className={`${stylos.nav} ${showNav ? stylos.showNav : ''}`}>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
@@ -30,10 +32,10 @@ function Navbar() {
 
                 </ul>
             </nav>
-
+{/* 
             <div className={stylos.responsive} onClick={toggleNav}>
                 <FontAwesomeIcon icon={faBars} />
-            </div>
+            </div> */}
         </header>
     );
 }
